6c4e8edf43459db2c0759fafa4aaf9718449a4b5
armandwipangestu <armandwi.pangestu7@gmail.com>
2026-03-19 18:16:14 +0700

    feat: add episode-19 manifest file

---
+++ episode-19/api-service/server.js
@@ -0,0 +1,13 @@
+const http = require("http");
+const os = require("os");
+
+const hostname = os.hostname();
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { "Content-Type": "text/plain" });
+  res.end(`Hello from API service running on pod ${hostname}\n`);
+});
+
+server.listen(3000, () => {
+  console.log("Server running at http://localhost:3000/");
+});
